656b6286d3c3b95091d4959e1ecd97772d35b0f6
Andrew Blair <andrewjblair@gmail.com>
2025-09-17 00:46:20 -0400

    add game screen and basic game loop

---
+++ constants.py
@@ -0,0 +1,13 @@
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+ASTEROID_MIN_RADIUS = 20
+ASTEROID_KINDS = 3
+ASTEROID_SPAWN_RATE = 0.8  # seconds
+ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS
+
+STARTUP_TEXT = (
+        "Starting Asteroids!\n"
+        f"Screen width: {SCREEN_WIDTH}\n"
+        f"Screen height: {SCREEN_HEIGHT}"
+    )
--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import pygame
+import constants
+
+
+def get_screen() -> pygame.SurfaceType:
+    screen = pygame.display.set_mode(
+        size=(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT),
+    )
+    return screen
+
+def game_loop(screen):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 def main():
-    print("Hello from asteroids!")
+    pygame.init()
+
+    print(constants.STARTUP_TEXT)
+
+    game_loop(get_screen())
 
 
 if __name__ == "__main__":
